668437f08221825adca5cdf56a7061e4a1420884
RickRo77 <rrickyroger@gmail.com>
2024-11-18 07:04:15 +0000

    .

--- src/main/java/Pack/SimulationUI.java
+++ src/main/java/Pack/SimulationUI.java
@@ -1,1 +1,19 @@
 package Pack;
+
+
+//start simulation
+//add emitter-emitter type
+//drag eimtter
+//change spread,speed,angle
+//delete emitter
+
+//add fieldpoint-select field type
+//change position
+//change strength
+//delete field
+
+//pause simulation
+//clear particles
+
+//save preset
+//load preset
