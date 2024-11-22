329f408fce95b3cfbe058736b50d04d019206dc4
Subhash H <144612968+Subhashhari@users.noreply.github.com>
2024-11-22 11:58:56 +0530

    Delete src/main/java/Pack directory

--- src/main/java/Pack/Main.java
+++
@@ -1,9 +0,0 @@
-package Pack;
-
-public class Main
-{
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        ParticleSystem ps = new ParticleSystem();   
-    }    
-}
